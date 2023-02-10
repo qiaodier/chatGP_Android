0d3c3e6b6674f9725d232351c01b6f3b500e0db4
qiaodier <dearliuyaning@163.com>
2023-02-10 15:02:26 +0800

    修改key

--- app/src/main/java/com/chat/demo/mvvm/repository/api/MainApi.kt
+++ app/src/main/java/com/chat/demo/mvvm/repository/api/MainApi.kt
@@ -10,7 +10,7 @@ import retrofit2.http.POST
 interface MainApi {
     @Headers(
         "Content-Type:application/json",
-        "Authorization:Bearer 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")
+        "Authorization:Bearer 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")
     @POST("v1/completions")
     suspend fun getAiContent(@Body req: AiReq): AiResp
 }
