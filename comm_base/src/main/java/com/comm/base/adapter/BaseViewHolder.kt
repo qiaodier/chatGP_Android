84ecc566c16d6121587f54b3c84d42ab14a6e50c
qiaohao <dearliuyaning@163.com>
2023-07-10 09:18:48 +0800

    新增适配封装

--- comm_base/src/main/java/com/comm/base/adapter/BaseViewHolder.kt
+++ comm_base/src/main/java/com/comm/base/adapter/BaseViewHolder.kt
@@ -3,5 +3,6 @@ package com.comm.base.adapter
 import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
 
+@Deprecated("")
 open class BaseViewHolder<T : ViewDataBinding>(val dataBinding: T) :
     RecyclerView.ViewHolder(dataBinding.root)
--- comm_base/src/main/java/com/comm/base/adapter/BaseViewNewAdapter.kt
+++ comm_base/src/main/java/com/comm/base/adapter/BaseViewNewAdapter.kt
@@ -1,65 +1,57 @@
 package com.comm.base.adapter
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
-import com.comm.base.extensions.getViewDataBinding
+import java.lang.reflect.ParameterizedType
 
 /**
  * V : ItemLayout 对应的 DataBinding
  * T : 实现类用到的实体类
  * VH: ItemLayout 对应的 ViewHolder
  */
-@Deprecated("可使用替代类'BaseViewNewAdapter'")
-abstract class BaseViewAdapter<V :ViewDataBinding,T, VH : BaseViewHolder<V>> : RecyclerView.Adapter<VH>() {
+abstract class BaseViewNewAdapter<V :ViewDataBinding,B> : RecyclerView.Adapter<BaseViewHolder<V>>() {
     /**
      * 布局id
-     *
      */
     abstract fun layoutId(): Int
 
     /**
      * 绑定数据
      */
-    abstract fun bindViewHolder(holder: VH, position: Int, data: T)
-
-    /**
-     * 创建viewHolder
-     */
-    abstract fun returnViewHolder(viewDataBinding: V):VH
+    abstract fun bindViewHolder(holder: V, position: Int, data: B)
 
     //数据集合
-    var dataList: MutableList<T> = mutableListOf()
+    var dataList: MutableList<B> = mutableListOf()
 
     /**
      * 内部已经调用了notify
      */
-    fun setData(sources: List<T>) {
-        dataList = sources.toMutableList()
+    fun setData(sources: MutableList<B>) {
+        dataList = sources
         notifyDataSetChanged()
     }
 
-    fun addData(count: Int, data: List<T>) {
+    fun addData(data: List<B>) {
         val start = dataList.count()
         dataList.addAll(data)
         notifyItemRangeInserted(start,data.size)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<V> {
         val binding = DataBindingUtil.inflate<V>(
             LayoutInflater.from(parent.context),
             layoutId(),
             parent,
             false
         )
-        return returnViewHolder(binding)
+        return BaseViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: VH, position: Int) {
-        bindViewHolder(holder, position, dataList[position])
+    override fun onBindViewHolder(holder: BaseViewHolder<V>, position: Int) {
+        bindViewHolder(holder.dataBinding, position, dataList[position])
     }
 
     override fun getItemCount(): Int {
